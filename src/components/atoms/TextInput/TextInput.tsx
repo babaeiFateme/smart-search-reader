e40ae334d317683eed23e32ff7f27387ab79c769
fatemeh <babaei.f1996@gmail.com>
2025-05-26 00:29:16 +0330

    refactor: add hover style for input

--- src/components/atoms/TextInput/TextInput.tsx
+++ src/components/atoms/TextInput/TextInput.tsx
@@ -1,5 +1,5 @@
 import type React from "react";
-import type { ITextInput } from "../../../core/types/textInput.type";
+import type { ITextInput } from "../../../core/types/TextInput.type";
 
 const TextInput: React.FC<ITextInput> = ({ className, ...props }) => {
     return <input {...props} className={className} type="text" />;
